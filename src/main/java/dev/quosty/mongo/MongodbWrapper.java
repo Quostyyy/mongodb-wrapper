138901c366f52df816fa7266d884cb3a052ae8da
Artur Kołecki <61193738+arthurr0@users.noreply.github.com>
2022-09-10 14:13:44 +0200

    Created a few new methods for MongodbImpl.

--- src/main/java/dev/quosty/mongo/MongodbWrapper.java
+++ src/main/java/dev/quosty/mongo/MongodbWrapper.java
@@ -9,42 +9,64 @@ import com.mongodb.client.model.Filters;
 import dev.quosty.mongo.adapter.AnnotationExclusionStrategy;
 import dev.quosty.mongo.annotations.MongodbEntity;
 import dev.quosty.mongo.impl.MongodbImpl;
+import java.util.List;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 public class MongodbWrapper implements MongodbImpl {
 
   private final MongoClient mongoClient;
+
+  private final Gson gson = new GsonBuilder()
+      .setExclusionStrategies(new AnnotationExclusionStrategy())
+      .serializeNulls()
+      .create();
 
   public MongodbWrapper(String mongodbUri) {
     this.mongoClient = MongoClients.create(mongodbUri);
   }
 
   @Override
-  public <T> void insertOne(T t) {
-    this.getDatabaseCollection(t.getClass()).insertOne(Document.parse(this.gson.toJson(t)));
+  public <T> void insertOne(T value) {
+    this.getDatabaseCollection(value.getClass()).insertOne(Document.parse(this.gson.toJson(value)));
   }
 
   @Override
-  public <T> void deleteOne(T t) {
-    this.getDatabaseCollection(t.getClass()).findOneAndDelete(Filters.eq("_id", Document.parse(new GsonBuilder().create().toJson(t))));
+  public <T> void deleteOne(T value) {
+    this.getDatabaseCollection(value.getClass()).findOneAndDelete(Filters.eq("_id", Document.parse(this.gson.toJson(value))));
   }
 
   @Override
-  public <T> void updateOne(T t) {
-    this.getDatabaseCollection(t.getClass()).findOneAndUpdate(Filters.eq("_id"), Document.parse(this.gson.toJson(t)));
+  public <T> void deleteMany(T value, Bson filters) {
+    this.getDatabaseCollection(value.getClass()).deleteMany(filters);
   }
 
   @Override
-  public MongoCollection<Document> getDatabaseCollection(Class<?> t) {
-    return !t.isAnnotationPresent(MongodbEntity.class) ? null : mongoClient
-        .getDatabase(t.getAnnotation(MongodbEntity.class).database())
-        .getCollection(t.getAnnotation(MongodbEntity.class).collection());
+  public <T> void updateOne(T value) {
+    this.getDatabaseCollection(value.getClass()).findOneAndUpdate(Filters.eq("_id"), Document.parse(this.gson.toJson(value)));
   }
 
-  private final Gson gson = new GsonBuilder()
-      .setExclusionStrategies(new AnnotationExclusionStrategy())
-      .serializeNulls()
-      .create();
+  @Override
+  public <T> void updateOne(T value, Bson filters) {
+    this.getDatabaseCollection(value.getClass()).findOneAndUpdate(filters, Document.parse(this.gson.toJson(value)));
+  }
+
+  @Override
+  public <T> void updateMany(List<T> value) {
+    value.forEach(this::updateOne);
+  }
+
+  @Override
+  public <T> void updateMany(List<T> value, Bson filters) {
+    value.forEach(object -> this.updateOne(object, filters));
+  }
+
+  @Override
+  public MongoCollection<Document> getDatabaseCollection(Class<?> value) {
+    return !value.isAnnotationPresent(MongodbEntity.class) ? null : mongoClient
+        .getDatabase(value.getAnnotation(MongodbEntity.class).database())
+        .getCollection(value.getAnnotation(MongodbEntity.class).collection());
+  }
 
   public MongoClient getMongodbClient() {
     return mongoClient;
