a0464cf98726042197a127d31ab81f801b6db6d3
Artur Kołecki <61193738+arthurr0@users.noreply.github.com>
2022-09-10 14:28:00 +0200

    Created a few new methods for MongodbImpl. (#2)
    * Created a few new methods for MongodbImpl.
    
    * Updated README.md with new methods.

--- src/main/java/dev/quosty/mongo/impl/MongodbImpl.java
+++ src/main/java/dev/quosty/mongo/impl/MongodbImpl.java
@@ -1,16 +1,26 @@
 package dev.quosty.mongo.impl;
 
 import com.mongodb.client.MongoCollection;
+import java.util.List;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 public interface MongodbImpl {
 
-  <T> void insertOne(T t);
+  <T> void insertOne(T value);
 
-  <T> void deleteOne(T t);
+  <T> void deleteOne(T value);
 
-  <T> void updateOne(T t);
+  <T> void deleteMany(T value, Bson filters);
 
-  MongoCollection<Document> getDatabaseCollection(Class<?> t);
+  <T> void updateOne(T value);
+
+  <T> void updateOne(T value, Bson filters);
+
+  <T> void updateMany(List<T> value);
+
+  <T> void updateMany(List<T> value, Bson filters);
+
+  MongoCollection<Document> getDatabaseCollection(Class<?> value);
 
 }
